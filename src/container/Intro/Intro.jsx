b3d72a7a919ec9b5ce7603698ab1fa74c02b92d3
Begimai Abdybekova <b.abdybekova@gmail.com>
2022-12-07 20:01:29 -0600

    commit

---
+++ src/container/Intro/Intro.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { MdVideoCameraFront } from "react-icons/md";
+
+import { meal } from "../../constants";
+import "./Intro.css";
+
+const Intro = () => {
+  const [palyVideo, setPlayVideo] = useState(false);
+  const vidRef = React.useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (palyVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        type="video/mp4"
+        ref={vidRef}
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {palyVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Intro;

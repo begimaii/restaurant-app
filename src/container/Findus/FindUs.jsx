6c8d1c250c378cb050569d5c659b8d59640dc9ba
Begimai Abdybekova <b.abdybekova@gmail.com>
2023-01-03 22:01:02 -0600

    findus

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info"></div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
